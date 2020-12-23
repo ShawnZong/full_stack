f3e499c88d056af874067166f3890d11be2f9e73
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-23 21:51:35 +0200

    add2.10 refract components

--- part2/phonebook/src/components/app.js
+++ part2/phonebook/src/components/app.js
@@ -1,5 +1,68 @@
 import React, { useState } from "react";
+const Filter = ({ setSearchName }) => {
+  const handleSearch = (event) => {
+    setSearchName(event.target.value);
+  };
+  return (
+    <div>
+      filter shown with
+      <input
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
+const PersonForm = (
+  { persons, setPersons, newName, setNewName, newPhone, setPhone },
+) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    if (persons.includes(persons.find((person) => person.name === newName))) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const newObj = { name: newName, phone: newPhone };
+      setPersons(persons.concat(newObj));
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        number:<input
+          onChange={(event) => {
+            setPhone(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+const Persons = ({ searchName, persons }) => {
+  const newPersons = persons.filter((person) => {
+    if (person.name.toLocaleLowerCase().includes(searchName)) {
+      return true;
+    }
+    return false;
+  });
+
+  return (<div>
+    <ul>
+      {newPersons.map((person) =>
+        <li key={person.name}>{person.name} {person.phone}</li>
+      )}
+    </ul>
+  </div>);
+};
 const App = () => {
   const [persons, setPersons] = useState(
     [
@@ -12,62 +75,22 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newPhone, setPhone] = useState("");
   const [searchName, setSearchName] = useState("");
-  const handleSubmit = (event) => {
-    event.preventDefault();
 
-    if (persons.includes(persons.find((person) => person.name === newName))) {
-      window.alert(`${newName} is already added to phonebook`);
-    } else {
-      const newObj = { name: newName, phone: newPhone };
-      setPersons(persons.concat(newObj));
-    }
-  };
-  const handleSearch = (event) => {
-    setSearchName(event.target.value);
-  };
-  const Show = () => {
-    const newPersons = persons.filter((person) => {
-      if (person.name.toLocaleLowerCase().includes(searchName)) {
-        return true;
-      }
-      return false;
-    });
-    return newPersons.map((person) =>
-      <li key={person.name}>{person.name} {person.phone}</li>
-    );
-  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with<input
-          onChange={handleSearch}
-        />
-      </div>
+      <Filter setSearchName={setSearchName} />
       <h2>add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input
-            onChange={(event) => {
-              setNewName(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          number:<input
-            onChange={(event) => {
-              setPhone(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        newPhone={newPhone}
+        setNewName={setNewName}
+        setPhone={setPhone}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {Show()}
-      </ul>
+      <Persons searchName={searchName} persons={persons} />
     </div>
   );
 };
