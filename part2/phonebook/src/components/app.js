cadf22c3c276d1ed266eac2635d982eff90026b0
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-23 20:56:16 +0200

    2.6 phonebook add person

---
+++ part2/phonebook/src/components/app.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newObj = { name: newName };
+    setPersons(persons.concat(newObj));
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>debug: {newName}</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input
+            onChange={(event) => {
+              setNewName(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => <li>{person.name}</li>)}
+      </ul>
+      ...
+    </div>
+  );
+};
+
+export default App;
