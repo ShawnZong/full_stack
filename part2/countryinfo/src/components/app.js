24c492675ffa0d82d3eb831e143c9500bc5d4353
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-25 05:23:21 +0200

    single weather not working

--- part2/countryinfo/src/components/app.js
+++ part2/countryinfo/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+const api_key = process.env.REACT_APP_API_KEY;
 const Filter = ({ setSearchName }) => {
   const handleSearch = (event) => {
     setSearchName(event.target.value);
@@ -14,7 +15,7 @@ const Filter = ({ setSearchName }) => {
   );
 };
 const ShowCountryName = ({ country }) => <p>{country.name}</p>;
-const ShowCountryDetail = ({ country }) => {
+const ShowCountryDetail = ({ country, weather }) => {
   return (
     <div>
       <h1>{country.name}</h1>
@@ -26,11 +27,22 @@ const ShowCountryDetail = ({ country }) => {
           <li key={language.name}>{language.name}</li>
         )}
       </ul>
-      <img src={country.flag} alt={country.name} width="500" height="400" />
+      <img src={country.flag} alt={country.name} width="200" height="100" />
+      <h2>Weather in {country.capital}</h2>
+      <p><strong>temperature:</strong>{weather.current.temperature} Celcius</p>
+
+      <img
+        src={weather.current.weather_icons[0]}
+        alt={weather.current.weather_descriptions[0]}
+      />
+      <p>
+        <strong>wind:</strong>
+        {weather.current.wind_speed} mph direction SSW
+      </p>
     </div>
   );
 };
-const ViewButton = ({ index, viewStates, setViewStates, country }) => {
+const ViewButton = ({ index, viewStates, setViewStates, country, weather }) => {
   const handleViewButton = () => {
     const tmp = [...viewStates];
     tmp[index] = !tmp[index];
@@ -40,7 +52,7 @@ const ViewButton = ({ index, viewStates, setViewStates, country }) => {
     return (
       <div>
         <button onClick={handleViewButton}>hide</button>
-        <ShowCountryDetail country={country} />
+        <ShowCountryDetail country={country} weather={weather} />
       </div>
     );
   } else {
@@ -51,10 +63,13 @@ const ViewButton = ({ index, viewStates, setViewStates, country }) => {
     );
   }
 };
-const ShowCountries = ({ countries, viewStates, setViewStates }) => {
+const ShowCountries = (
+  { countries, viewStates, setViewStates, weathers },
+) => {
+  console.log(weathers);
   if (countries.length === 1) {
     return (
-      <ShowCountryDetail country={countries[0]} />
+      <ShowCountryDetail country={countries[0]} weather={weathers[0]} />
     );
   } else if (countries.length <= 10) {
     return (
@@ -68,6 +83,7 @@ const ShowCountries = ({ countries, viewStates, setViewStates }) => {
                 viewStates={viewStates}
                 setViewStates={setViewStates}
                 country={country}
+                weather={weathers[index]}
               />
             </div>
           );
@@ -86,6 +102,8 @@ const App = () => {
   const [searchName, setSearchName] = useState("");
   const [countries, setCountries] = useState([]);
   const [viewStates, setViewStates] = useState([]);
+  const [weathers, setWeathers] = useState([]);
+
   useEffect(() => {
     if (searchName !== "") {
       const searchURL = `https://restcountries.eu/rest/v2/name/${searchName}`;
@@ -93,13 +111,59 @@ const App = () => {
         .get(searchURL)
         .then((response) => {
           setCountries(response.data);
-          setViewStates(new Array(response.data.length).fill(false));
+          console.log("country", response.data);
+          console.log("data length", response.data.length);
+          if (response.data.length <= 10) {
+            setViewStates(new Array(response.data.length).fill(false));
+
+            setWeathers(new Array(response.data.length).fill({}));
+            let weatherURL = "";
+            const weatherTmp = [];
+            response.data.forEach((country) => {
+              console.log("enter foreach");
+              weatherURL =
+                `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`;
+              axios
+                .get(weatherURL)
+                .then((response) => {
+                  weatherTmp.push(JSON.parse(JSON.stringify(response.data)));
+                  // weatherTmp.push(JSON.parse(JSON.stringify(response.data)));
+                })
+                .catch((error) => console.log(error));
+            });
+            console.log("weathertmp", weatherTmp);
+            setWeathers(weatherTmp);
+          }
         })
         .catch((error) => {
           console.log(error);
         });
     }
   }, [searchName]);
+
+  // useEffect(() => {
+  //   if (countries.length > 0 && countries.length <= 10) {
+  //     setWeathers(new Array(countries.length).fill({}));
+  //     let weatherURL = "";
+  //     const weatherTmp = [];
+
+  //     countries.forEach((country) => {
+  //       weatherURL =
+  //         `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`;
+  //       axios
+  //         .get(weatherURL)
+  //         .then((response) => {
+  //           weatherTmp.push(JSON.parse(JSON.stringify(response.data)));
+  //           // weatherTmp.push(JSON.parse(JSON.stringify(response.data)));
+  //         })
+  //         .catch((error) => console.log(error));
+  //     });
+  //     console.log("weathertmp", weatherTmp);
+  //     setWeathers(weatherTmp);
+  //   }
+  // }, [countries]);
+  console.log("countries", countries);
+  console.log("weathers", weathers);
   if (countries.length > 0) {
     return (
       <div>
@@ -108,6 +172,7 @@ const App = () => {
           countries={countries}
           viewStates={viewStates}
           setViewStates={setViewStates}
+          weathers={weathers}
         />
       </div>
     );
