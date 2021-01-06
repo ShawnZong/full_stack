faf0b628a43bcf7f544ad87b5e3f29358c798a7a
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2021-01-06 19:24:45 +0200

    add6.1 Redux test

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,29 @@
+import lodash from 'lodash'
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  const stateTmp=lodash.cloneDeep(state)
+
+  switch (action.type) {
+  case 'GOOD':
+    stateTmp.good=stateTmp.good+1
+    return stateTmp
+  case 'OK':
+    stateTmp.ok=stateTmp.ok+1
+    return stateTmp
+  case 'BAD':
+    stateTmp.bad=stateTmp.bad+1
+    return stateTmp
+  case 'ZERO':
+    return 0
+  default: return state
+  }
+
+}
+
+export default counterReducer
